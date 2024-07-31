9b9613865c5c72567ce11d7b544a68515a8e544d
gebu8f <125874137+gebu8f@users.noreply.github.com>
2024-07-31 20:56:41 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -33,6 +33,12 @@ const workerConfig = {
       method: 'GET',
       tatget: 'https://gebu8f6479.pp.ua',
       },
+    {
+      id: 'my_server_monitor',
+      name: 'My Minecraft server',
+      mehod: 'TCP_PING',
+      target: 'papaya.fps.ms:26300',
+      },
   ],
   notification: {
     // [Optional] apprise API server URL
